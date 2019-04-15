abea3131a02b7f5f5c8499cae246df14f3b205f6
Zak Kolar <zak.kolar@gmail.com>
2019-04-14 20:20:43 -0500

    updated blocks plugin layout

--- _plugins/scratch_blocks.rb
+++ _plugins/scratch_blocks.rb
@@ -12,7 +12,7 @@ module Jekyll
       "
 <div class='scratchblocks-embed'>
 <pre class='blocks' aria-label='#{escape text.gsub("'","&#39;").gsub("\"","&quot;")}'>#{text}</pre>
-<p class='scratchblocks-link'><a href='https://scratchblocks.github.io/#?style=scratch3&script="+CGI.escape(text).gsub('+','%20')+"' target='_blank'>Edit code on Scratch Blocks</a></p>
+<p class='scratchblocks-link'><a href='https://scratchblocks.github.io/#?style=scratch3&script="+CGI.escape(text).gsub('+','%20')+"' target='_blank'>Edit blocks</a></p>
 <noscript>View this page in a web browser to see the Scratch blocks.</noscript>
 </div>
 "

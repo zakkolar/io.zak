3f73864ba847d8ffcc99df81bc85eaa2547ff0c0
Zak Kolar <zak.kolar@gmail.com>
2019-05-20 18:27:08 -0400

    fixed hash issue

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -66,63 +66,63 @@ $(document).ready(function() {
     // Smooth scrolling
 
     // Bind popstate event listener to support back/forward buttons.
-    var smoothScrolling = false;
-    $(window).bind("popstate", function (event) {
-        $.smoothScroll({
-            scrollTarget: location.hash,
-            offset: -20,
-            beforeScroll: function() { smoothScrolling = true; },
-            afterScroll: function() { smoothScrolling = false; }
-        });
-    });
-    // Override clicking on links to smooth scroll
-    $('a[href*="#"]').bind("click", function (event) {
-        if (this.pathname === location.pathname && this.hash) {
-            event.preventDefault();
-            history.pushState(null, null, this.hash);
-            $(window).trigger("popstate");
-        }
-    });
-    // Smooth scroll on page load if there is a hash in the URL.
-    if (location.hash) {
-        $(window).trigger("popstate");
-    }
-
-    // Scrollspy equivalent: update hash fragment while scrolling.
-    $(window).scroll(jQuery.throttle(250, function() {
-        // Don't run while smooth scrolling (from clicking on a link).
-        if (smoothScrolling) return;
-        var scrollTop = $(window).scrollTop() + 20 + 1;  // 20 = offset
-        var links = [];
-        $("nav.toc a").each(function() {
-            var link = $(this);
-            var href = link.attr("href");
-            if (href && href[0] == "#") {
-                var element = $(href);
-                links.push({
-                    link: link,
-                    href: href,
-                    top: element.offset().top
-                });
-                link.removeClass('active');
-            }
-        });
-        for (var i = 0; i < links.length; i++) {
-            var top = links[i].top;
-            var bottom = (i < links.length - 1 ? links[i+1].top : Infinity);
-            if (top <= scrollTop && scrollTop < bottom) {
-                // Mark all ancestors as active
-                links[i].link.parents("li").children("a").addClass('active');
-                if (links[i].href !== location.hash) {
-                    history.replaceState(null, null, links[i].href);
-                }
-                return;
-            }
-        }
-        if ('#' !== location.hash) {
-            history.replaceState(null, null, '#');
-        }
-    }));
+    // var smoothScrolling = false;
+    // $(window).bind("popstate", function (event) {
+    //     $.smoothScroll({
+    //         scrollTarget: location.hash,
+    //         offset: -20,
+    //         beforeScroll: function() { smoothScrolling = true; },
+    //         afterScroll: function() { smoothScrolling = false; }
+    //     });
+    // });
+    // // Override clicking on links to smooth scroll
+    // $('a[href*="#"]').bind("click", function (event) {
+    //     if (this.pathname === location.pathname && this.hash) {
+    //         event.preventDefault();
+    //         history.pushState(null, null, this.hash);
+    //         $(window).trigger("popstate");
+    //     }
+    // });
+    // // Smooth scroll on page load if there is a hash in the URL.
+    // if (location.hash) {
+    //     $(window).trigger("popstate");
+    // }
+    //
+    // // Scrollspy equivalent: update hash fragment while scrolling.
+    // $(window).scroll(jQuery.throttle(250, function() {
+    //     // Don't run while smooth scrolling (from clicking on a link).
+    //     if (smoothScrolling) return;
+    //     var scrollTop = $(window).scrollTop() + 20 + 1;  // 20 = offset
+    //     var links = [];
+    //     $("nav.toc a").each(function() {
+    //         var link = $(this);
+    //         var href = link.attr("href");
+    //         if (href && href[0] == "#") {
+    //             var element = $(href);
+    //             links.push({
+    //                 link: link,
+    //                 href: href,
+    //                 top: element.offset().top
+    //             });
+    //             link.removeClass('active');
+    //         }
+    //     });
+    //     for (var i = 0; i < links.length; i++) {
+    //         var top = links[i].top;
+    //         var bottom = (i < links.length - 1 ? links[i+1].top : Infinity);
+    //         if (top <= scrollTop && scrollTop < bottom) {
+    //             // Mark all ancestors as active
+    //             links[i].link.parents("li").children("a").addClass('active');
+    //             if (links[i].href !== location.hash) {
+    //                 history.replaceState(null, null, links[i].href);
+    //             }
+    //             return;
+    //         }
+    //     }
+    //     if ('#' !== location.hash) {
+    //         history.replaceState(null, null, '#');
+    //     }
+    // }));
 
     // add lightbox class to all image links
     $(
